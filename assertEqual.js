c815042d193856b07212466364c1b551e5964ceb
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-08-17 23:46:21 +0000

    assert js

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("❌❌❌Assertion Failed: " + '"' + actual + '"' + " !== " + '"' + expected + '"');
+  } else {
+    console.log("✅✅✅Assertion Passed: " + '"' + actual + '"' + " === " + '"' + expected + '"');
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
