8b2c6018996bac6177d586e54fdebc65f145dd9d
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-08-18 23:40:33 +0000

    create assertArrayEqual that calls assertEqual and prints to console

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(array1, array2){
+  if(array1.length !== array2.length) {
+    return false;
+  };
+  for(let i = 0; i < array1.length; i++){
+    if(array1[i] !== array2[i]){
+      return false;
+    }
+  };
+  return true;
+};
+
+const assertArrayEqual = function(arr1, arr2) {
+  if(eqArrays(arr1,arr2)){
+    console.log(`${arr1} === ${arr2} , this solution passed`);
+  }else {
+    console.log(`${arr1} !== ${arr2} , this solution failed`);
+    }
+
+  };
+
+  assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
