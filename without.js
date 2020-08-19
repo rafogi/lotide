5491ba87b7d6fd1d39ab18d1cacca9cc641a2470
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-08-19 00:41:43 +0000

    adding without function to lotide

---
+++ without.js
@@ -0,0 +1,39 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  };
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  };
+  return true;
+};
+
+const assertArrayEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`${arr1} === ${arr2} , this solution passed`);
+  } else {
+    console.log(`${arr1} !== ${arr2} , this solution failed`);
+  }
+
+};
+
+
+const without = function (ar1, ar2) {
+  let newAr = ar1;
+  for (i = 0; i < ar1.length; i++) {
+    for (j = 0; j < ar2.length; j++) {
+      if (ar1[i] === ar2[j]) {
+        newAr.splice(i, 1);
+      }
+
+    }
+  }
+  return newAr;
+}
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArrayEqual(words, ["hello", "world", "lighthouse"]);
+
